b705299a1b9f9183f6ed75a2d033da5834dc8d8a
andrewoc1 <andrewoc1111@gmail.com>
2026-06-10 18:16:52 -0400

    Implement computer and human choices

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+var humanScore = 0;
+var computerScore = 0;
+
+
+
+
+
 function getComputerChoice(){
 	let choice = Math.floor(Math.random() * 3);
 
@@ -16,6 +24,48 @@ function getComputerChoice(){
 
 
 console.log(getComputerChoice());
+
+function getHumanChoice(){
+	let choice = prompt("Choose 1 for Rock, 2 for Scissors, and 3 for Paper!");
+	let valueChosen = "";
+	
+	if (choice == 1){
+		valueChosen = "rock";
+	}else if(choice == 2){
+		valueChosen = "scissors";
+	}else{
+		valueChosen = "paper";
+	}
+
+	return valueChosen;
+}
+
+
+console.log((getHumanChoice()));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
